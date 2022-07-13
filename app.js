a65c78e3757453fe0022b4e51578065c3235fb96
ikechukwu <ikechukwu@brandmobile.net>
2022-07-13 16:48:20 +0100

    error 5** corrected

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 const express = require('express')
 const cookieParser = require('cookie-parser')
-var cors = require('cors')
+// var cors = require('cors')
 const app = express()
 
 
-const corsOpts = {
-    origin: '*',
+// const corsOpts = {
+//     origin: '*',
 
-    methods: [
-        'GET',
-        'POST',
-    ],
+//     methods: [
+//         'GET',
+//         'POST',
+//     ],
 
-    allowedHeaders: [
-        'Content-Type',
-    ],
-};
+//     allowedHeaders: [
+//         'Content-Type',
+//     ],
+// };
 
 require('dotenv').config()
 const currentPort = process.env.PORT || 1950
@@ -23,7 +23,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
-app.use(cors(corsOpts))
+// app.use(cors(corsOpts))
 
 app.use('/v1/lms/course', require('./src/routers/courses/Course'))
 app.use('/v1/lms/quiz', require('./src/routers/quiz/Quiz'))
